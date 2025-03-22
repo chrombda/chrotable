faed3b14164e96936df61b0c4cf2b1db833c2ffb
koyeo <cansen.hu@certik.com>
2025-03-22 21:34:16 +0800

    feat: option

--- chrotable.go
+++ chrotable.go
@@ -2,25 +2,46 @@ package chrotable
 
 import "github.com/elliotchance/orderedmap/v3"
 
+type Option[T any] func(*config[T])
+
+type config[T any] struct {
+	convertor func(item T) []Cell
+}
+
+func WithConvertor[T any](convertor func(item T) []Cell) Option[T] {
+	return func(c *config[T]) {
+		c.convertor = convertor
+	}
+}
+
 type Chrotable[T any] struct {
+	config    *config[T]
 	columns   []Column
 	constants *orderedmap.OrderedMap[string, any]
 	variables *orderedmap.OrderedMap[string, any]
 	rows      [][]Cell
-	convertor func(item T) []Cell
+}
+
+func NewChrotable[T any](options ...Option[T]) *Chrotable[T] {
+	c := &Chrotable[T]{
+		config: &config[T]{},
+	}
+	for _, option := range options {
+		option(c.config)
+	}
+	return c
 }
 
 func (c *Chrotable[T]) init() {
+	if c.config == nil {
+		c.config = &config[T]{}
+	}
 	if c.constants == nil {
 		c.constants = orderedmap.NewOrderedMap[string, any]()
 	}
 	if c.variables == nil {
 		c.variables = orderedmap.NewOrderedMap[string, any]()
 	}
-}
-
-func (c *Chrotable[T]) SetConvertor(convertor func(item T) []Cell) {
-	c.convertor = convertor
 }
 
 func (c *Chrotable[T]) SetColumns(columns []Column) {
